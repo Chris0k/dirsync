1e6a356e70d441ff7c34861070a9fda943d46658
chrisok <35911855+Chris0k@users.noreply.github.com>
2025-02-23 13:35:00 +0100

    WIP

--- dirsync.sh
+++ dirsync.sh
@@ -4,8 +4,11 @@ source=$1
 destination=$2
 verbose=${3:-false}
 
+sync_ignore_file="./sync_ignore.txt"
+
 [ "$verbose" = true ] && inotify_params="-r" || inotify_params="-rqq"
 [ "$verbose" = true ] && rsync_params="-avz" || rsync_params="-az"
+[ -f $sync_ignore_file ] && rsync_params="${rsync_params} --exclude-from $sync_ignore_file"
 
 while inotifywait "$inotify_params" -e modify,create,delete,move "$source"; do
     rsync "$rsync_params" --del "$source" "$destination"
